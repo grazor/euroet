00c9706ab24c9d4d230aa8e41c02cbe2c214e264
Sergey Porivaev <porivaevs@gmail.com>
2019-03-10 12:04:59 +0300

    Refactory tests

--- back/tests/test_api/test_product.py
+++ back/tests/test_api/test_product.py
@@ -5,69 +5,60 @@ from server.users.factories import AdminFactory, ManagerFactory
 
 
 def test_products_list_has_permission(db, api_client):
-    user = ManagerFactory()
-    p1 = ProjectFactory(created_by=user)
+    project = ProjectFactory(created_by=ManagerFactory())
 
-    url = reverse('product-list', kwargs={'project_slug': p1.slug})
-    api_client.force_authenticate(user=user)
+    url = reverse('product-list', kwargs={'project_slug': project.slug})
+    api_client.force_authenticate(user=project.created_by)
     response = api_client.get(url)
     assert response.status_code == 200, 'Can not access own products'
 
 
 def test_products_list_no_permission(db, api_client):
-    user1 = ManagerFactory()
-    user2 = ManagerFactory()
-    p2 = ProjectFactory(created_by=user2)
+    user = ManagerFactory()
+    project = ProjectFactory()
 
-    url = reverse('product-list', kwargs={'project_slug': p2.slug})
-    api_client.force_authenticate(user=user1)
+    url = reverse('product-list', kwargs={'project_slug': project.slug})
+    api_client.force_authenticate(user=user)
     response = api_client.get(url)
     assert response.status_code == 403, 'Can access someone else\'s project'
 
 
 def test_products_list_read_permission(db, api_client):
-    user1 = ManagerFactory()
-    user2 = ManagerFactory()
-    p2 = ProjectFactory(created_by=user2)
-    ReadProjectAccessFactory(user=user1, project=p2)
+    project = ProjectFactory(created_by=ManagerFactory())
+    access = ReadProjectAccessFactory(user=ManagerFactory(), project=project)
 
-    url = reverse('product-list', kwargs={'project_slug': p2.slug})
-    api_client.force_authenticate(user=user1)
+    url = reverse('product-list', kwargs={'project_slug': project.slug})
+    api_client.force_authenticate(user=access.user)
     response = api_client.get(url)
     assert response.status_code == 200, 'Read access does not work'
 
 
 def test_products_list_admin(db, api_client):
     admin = AdminFactory()
-    user1 = ManagerFactory()
-    p1 = ProjectFactory(created_by=user1)
+    project = ProjectFactory(created_by=ManagerFactory())
 
-    url = reverse('product-list', kwargs={'project_slug': p1.slug})
+    url = reverse('product-list', kwargs={'project_slug': project.slug})
     api_client.force_authenticate(user=admin)
     response = api_client.get(url)
     assert response.status_code == 200, 'Admin has no access to project'
 
 
 def test_product_add_write_permission(db, api_client):
-    user1 = ManagerFactory()
-    user2 = ManagerFactory()
-    p2 = ProjectFactory(created_by=user2)
-    WriteProjectAccessFactory(user=user1, project=p2)
+    project = ProjectFactory(created_by=ManagerFactory())
+    access = WriteProjectAccessFactory(user=ManagerFactory(), project=project)
 
-    url = reverse('product-list', kwargs={'project_slug': p2.slug})
-    api_client.force_authenticate(user=user1)
+    url = reverse('product-list', kwargs={'project_slug': project.slug})
+    api_client.force_authenticate(user=access.user)
     response = api_client.post(url, data={'slug': 'test', 'name': 'test'})
-    assert response.status_code == 201, 'Read access does not work'
-    assert p2.products.get().slug == 'test'
+    assert response.status_code == 201, 'Write access does not work'
+    assert project.products.get().slug == 'test'
 
 
 def test_product_add_read_permission_fails(db, api_client):
-    user1 = ManagerFactory()
-    user2 = ManagerFactory()
-    p2 = ProjectFactory(created_by=user2)
-    ReadProjectAccessFactory(user=user1, project=p2)
+    project = ProjectFactory(created_by=ManagerFactory())
+    access = ReadProjectAccessFactory(user=ManagerFactory(), project=project)
 
-    url = reverse('product-list', kwargs={'project_slug': p2.slug})
-    api_client.force_authenticate(user=user1)
+    url = reverse('product-list', kwargs={'project_slug': project.slug})
+    api_client.force_authenticate(user=access.user)
     response = api_client.post(url, data={'slug': 'test', 'name': 'test'})
     assert response.status_code == 403, 'Can create products with read permission'
--- back/tests/test_api/test_project.py
+++ back/tests/test_api/test_project.py
@@ -19,79 +19,58 @@ def test_project_ownership(db):
 
 
 def test_project_list_as_admin(db, api_client):
-    admin = AdminFactory()
-
-    p1 = ProjectFactory(created_by=admin)
-    p2 = ProjectFactory(created_by=ManagerFactory())
+    project1 = ProjectFactory(created_by=AdminFactory())
+    project2 = ProjectFactory(created_by=ManagerFactory())
 
     url = reverse('project-list')
-    api_client.force_authenticate(user=admin)
+    api_client.force_authenticate(user=project1.created_by)
     response = api_client.get(url)
-    assert response.status_code == 200
+    assert response.status_code == 200, 'Admin can not list all projects'
 
     data = response.json()
     assert len(data) == 2
-    assert {p1.slug, p2.slug} == {p['slug'] for p in data}
+    assert {project1.slug, project2.slug} == {p['slug'] for p in data}
 
 
 def test_project_list_as_user(db, api_client):
-    user = ManagerFactory()
-
+    project = ProjectFactory(created_by=ManagerFactory())
+    access = ReadProjectAccessFactory(user=project.created_by, project=ProjectFactory())
     ProjectFactory(created_by=AdminFactory())
-    p2 = ProjectFactory(created_by=user)
-    p3 = ProjectFactory(created_by=AdminFactory())
-    ReadProjectAccessFactory(user=user, project=p3)
 
     url = reverse('project-list')
-    api_client.force_authenticate(user=user)
+    api_client.force_authenticate(user=project.created_by)
     response = api_client.get(url)
-    assert response.status_code == 200
+    assert response.status_code == 200, 'User can not list projects'
 
     data = response.json()
-    assert len(data) == 2
-    assert {p2.slug, p3.slug} == {p['slug'] for p in data}
+    assert len(data) == 2, 'User permissions error'
+    assert {project.slug, access.project.slug} == {p['slug'] for p in data}
 
 
-def test_detail_allowed_only(db, api_client):
-    user1 = ManagerFactory()
-    user2 = ManagerFactory()
+def test_detail_access_own(db, api_client):
+    project = ProjectFactory(created_by=ManagerFactory())
 
-    p1 = ProjectFactory(created_by=user1)
-    p2 = ProjectFactory(created_by=user2)
-    p3 = ProjectFactory(created_by=user2)
-    ReadProjectAccessFactory(user=user1, project=p3)
-
-    api_client.force_authenticate(user=user1)
-
-    url = reverse('project-detail', kwargs={'pk': p1.slug})
+    api_client.force_authenticate(user=project.created_by)
+    url = reverse('project-detail', kwargs={'pk': project.slug})
     response = api_client.get(url)
     assert response.status_code == 200, 'Can not acces own project'
 
-    url = reverse('project-detail', kwargs={'pk': p2.slug})
+
+def test_detail_no_access_fails(db, api_client):
+    user = ManagerFactory()
+    project = ProjectFactory(created_by=ManagerFactory())
+
+    api_client.force_authenticate(user=user)
+    url = reverse('project-detail', kwargs={'pk': project.slug})
     response = api_client.get(url)
     assert response.status_code == 404, 'Can access soneone else\'s project'
 
-    url = reverse('project-detail', kwargs={'pk': p3.slug})
-    response = api_client.get(url)
-    assert response.status_code == 200, 'Can not access shared to read project'
 
-
-def test_update_with_write_access_only(db, api_client):
+def test_detail_read_access(db, api_client):
     user = ManagerFactory()
-    project = ProjectFactory(created_by=user)
-    access = project.access.get(user=user)
-    access.access_type = ProjectAccess.AccessType.read.name
-    access.save()
+    access = ReadProjectAccessFactory(user=user, project=ProjectFactory(created_by=ManagerFactory()))
 
     api_client.force_authenticate(user=user)
-
-    url = reverse('project-detail', kwargs={'pk': project.slug})
-    response = api_client.patch(url, data={'name': 'test_name'})
-    assert response.status_code == 403, 'Can modify someone else\'s project'
-
-    access.access_type = ProjectAccess.AccessType.write.name
-    access.save()
-    response = api_client.patch(url, data={'name': 'test_name2'})
-    project.refresh_from_db()
-    assert response.status_code == 200, 'Can not modify shared to write project'
-    assert project.name == 'test_name2'
+    url = reverse('project-detail', kwargs={'pk': access.project.slug})
+    response = api_client.get(url)
+    assert response.status_code == 200, 'Can not access shared to read project'
