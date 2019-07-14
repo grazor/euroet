5e053f2ecb67f9cb44792d2f7dabdcc89ba952d7
Sergey Porivaev <porivaevs@gmail.com>
2019-07-14 13:12:38 +0300

    Lints

--- back/server/urls.py
+++ back/server/urls.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""
-Main URL mapping configuration file.
-"""
+"""Main URL mapping configuration file."""
 
 from health_check import urls as health_urls
 
@@ -22,45 +20,29 @@ urlpatterns = [
     # Apps:
     path('api/users/', include(users_urls)),
     path('api/', include(pm_urls)),
-
     # Health checks:
     path('health/', include(health_urls)),  # noqa: DJ05
-
     # django-admin:
     path('admin/doc/', include(admindocs_urls)),  # noqa: DJ05
     path('admin/', admin.site.urls),
-
     # Text and xml static files:
-    path('robots.txt', TemplateView.as_view(
-        template_name='txt/robots.txt',
-        content_type='text/plain',
-    )),
-    path('humans.txt', TemplateView.as_view(
-        template_name='txt/humans.txt',
-        content_type='text/plain',
-    )),
+    path('robots.txt', TemplateView.as_view(template_name='txt/robots.txt', content_type='text/plain')),
+    path('humans.txt', TemplateView.as_view(template_name='txt/humans.txt', content_type='text/plain')),
 ]
 
 if settings.DEBUG:  # pragma: no cover
     import debug_toolbar  # noqa: Z435
     from django.views.static import serve  # noqa: Z435
-    from rest_framework.documentation import include_docs_urls
+    from rest_framework.documentation import include_docs_urls  # noqa: Z435
 
     urlpatterns = [
         # URLs specific only to django-debug-toolbar:
         path('__debug__/', include(debug_toolbar.urls)),
-
         # Serving media files in development only:
         path('media/(<path:path>)', serve, {'document_root': settings.MEDIA_ROOT}),
-
         # Drf doc
-        path(
-            'docs/', include_docs_urls(
-                title='Euroet API',
-                authentication_classes=[],
-                permission_classes=[],
-        ))
-] + urlpatterns
+        path('docs/', include_docs_urls(title='Euroet API', authentication_classes=[], permission_classes=[])),
+    ] + urlpatterns
 
 
 urlpatterns.append(re_path(r'^(?!admin|api|docs).*$', singlepage, name='index'))
