dcb8bd287967e0353f1d450b031c0816d346051e
Sergey Porivaev <porivaevs@gmail.com>
2020-01-13 22:29:31 +0300

    Add import legend

--- front/app/containers/ComponentImportPage/ImportArea.js
+++ front/app/containers/ComponentImportPage/ImportArea.js
@@ -6,17 +6,32 @@ import { compose } from 'redux';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
 const styles = theme => ({
   root: {
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing(2),
     paddingBottom: theme.spacing(2),
   },
+  legendRoot: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+  },
   area: {
     minHeight: '150px !important',
   },
   importButton: {
     marginTop: theme.spacing(2),
+  },
+  table: {
+    minWidth: 650,
   },
 });
 
@@ -40,32 +55,71 @@ class ImportArea extends Component {
     const { classes } = this.props;
 
     return (
-      <Paper elevation={3} className={classes.root}>
-        <DropzoneArea
-          onChange={this.handleChange}
-          acceptedFiles={[
-            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-            'text/csv',
-          ]}
-          dropzoneText="Select .xlsx or .csv files to import"
-          filesLimit={1}
-          useChipsForPreview
-          showFileNamesInPreview
-          showAlerts
-          dropzoneClass={classes.area}
-          key={this.state.key}
-        />
-        <Button
-          fullWidth
-          disabled={this.state.files.length === 0}
-          variant="outlined"
-          color="primary"
-          className={classes.importButton}
-          onClick={this.initiateImport}
-        >
-          Import
-        </Button>
-      </Paper>
+      <React.Fragment>
+        <Paper elevation={3} className={classes.legendRoot}>
+          <Table className={classes.table} aria-label="import file structure">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center"></TableCell>
+                <TableCell align="center">code</TableCell>
+                <TableCell align="center">name</TableCell>
+                <TableCell align="center">price</TableCell>
+                <TableCell align="center">description</TableCell>
+                <TableCell align="center">collection</TableCell>
+                <TableCell align="center">manufacturer</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow key="required">
+                <TableCell align="left">Required?</TableCell>
+                <TableCell align="center">Yes</TableCell>
+                <TableCell align="center">Yes</TableCell>
+                <TableCell align="center">Yes</TableCell>
+                <TableCell align="center">No</TableCell>
+                <TableCell align="center">No</TableCell>
+                <TableCell align="center">No</TableCell>
+              </TableRow>
+              <TableRow key="example">
+                <TableCell align="left">Example</TableCell>
+                <TableCell align="center">001490</TableCell>
+                <TableCell align="center">
+                  Nedbox Набор креплений для сухих перегородок для щитов{' '}
+                </TableCell>
+                <TableCell align="center">500.10</TableCell>
+                <TableCell align="center"></TableCell>
+                <TableCell align="center">Legrand</TableCell>
+                <TableCell align="center"></TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Paper>
+        <Paper elevation={3} className={classes.root}>
+          <DropzoneArea
+            onChange={this.handleChange}
+            acceptedFiles={[
+              'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+              'text/csv',
+            ]}
+            dropzoneText="Select .xlsx or .csv files to import"
+            filesLimit={1}
+            useChipsForPreview
+            showFileNamesInPreview
+            showAlerts
+            dropzoneClass={classes.area}
+            key={this.state.key}
+          />
+          <Button
+            fullWidth
+            disabled={this.state.files.length === 0}
+            variant="outlined"
+            color="primary"
+            className={classes.importButton}
+            onClick={this.initiateImport}
+          >
+            Import
+          </Button>
+        </Paper>
+      </React.Fragment>
     );
   }
 }
